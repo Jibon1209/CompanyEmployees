5624d65067f8d32ea1b03ba53d02840984084f03
Jibon Hossain <jessore007@outlook.com>
2024-12-10 13:16:19 +0600

    Global Error

---
+++ CompanyEmployees/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Entities.Models;
+using Shared.DataTransferObjects;
+
+namespace CompanyEmployees
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Company, CompanyDto>()
+            .ForCtorParam("FullAddress",
+                opt => opt.MapFrom((src) => $"{src.Address} {src.Country}"));
+        }
+    }
+}
--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,6 +1,8 @@
 using CompanyEmployees.Extensions;
+using Contracts;
+using Microsoft.AspNetCore.HttpOverrides;
 using NLog;
-
+//76 page done
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,17 +12,27 @@ builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.AddAutoMapper(typeof(Program));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if(app.Environment.IsDevelopment())
-    app.UseDeveloperExceptionPage();
-else
-    app.UseHsts();
+var logger = app.Services.GetRequiredService<ILoggerManager>();
+app.ConfigureExceptionHandler(logger);
 
+if (app.Environment.IsProduction())
+     app.UseHsts();
+
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
 app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
